3117e108002f4fa241c786e82a0ca60103d6e0e5
Tim Davis <contact@timdav.is>
2023-05-02 13:16:41 -0700

    rollback all transactions and close all connections during repo tests

--- todo-app/repository/todos_test.go
+++ todo-app/repository/todos_test.go
@@ -10,82 +10,94 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func startTransaction(t *testing.T) *sql.Tx {
+func withRollback(t *testing.T, f func(*sql.Tx)) {
 	conn, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	assert.Nil(t, err)
+	defer conn.Close()
 	tx, err := conn.Begin()
 	assert.Nil(t, err)
-	return tx
+	defer tx.Rollback()
+	f(tx)
 }
 
 func TestEmptyTableGetsNoRows(t *testing.T) {
-	r := NewTodosRepository(startTransaction(t))
-	todos, err := r.GetAll()
-	assert.Nil(t, err)
-	assert.Len(t, *todos, 0)
+	withRollback(t, func(tx *sql.Tx) {
+		r := NewTodosRepository(tx)
+		todos, err := r.GetAll()
+		assert.Nil(t, err)
+		assert.Len(t, *todos, 0)
+	})
 }
 
 func TestPopulatedTableGetsAllRows(t *testing.T) {
-	tx := startTransaction(t)
-	r := NewTodosRepository(tx)
-	_, err := tx.Exec(`--sql
-		insert into "todos" ("task")
-		values ('Learn Go'),
-			   ('Do a Barrel Roll'),
-			   ('Try a Somersault')
-	`)
-	assert.Nil(t, err)
-	todos, err := r.GetAll()
-	assert.Nil(t, err)
-	assert.Equal(t, len(*todos), 3)
+	withRollback(t, func(tx *sql.Tx) {
+		r := NewTodosRepository(tx)
+		_, err := tx.Exec(`--sql
+			insert into "todos" ("task")
+			values ('Learn Go'),
+				   ('Do a Barrel Roll'),
+				   ('Try a Somersault')
+		`)
+		assert.Nil(t, err)
+		todos, err := r.GetAll()
+		assert.Nil(t, err)
+		assert.Equal(t, len(*todos), 3)
+	})
 }
 
 func TestEmptyTableGetsOneNilTodo(t *testing.T) {
-	r := NewTodosRepository(startTransaction(t))
-	todo, err := r.GetOneByID(uuid.New())
-	assert.Nil(t, err)
-	assert.Nil(t, todo)
+	withRollback(t, func(tx *sql.Tx) {
+		r := NewTodosRepository(tx)
+		todo, err := r.GetOneByID(uuid.New())
+		assert.Nil(t, err)
+		assert.Nil(t, todo)
+	})
 }
 
 func TestEmptyTableUpdatesOneNilTodo(t *testing.T) {
-	r := NewTodosRepository(startTransaction(t))
-	todo, err := r.UpdateOne(uuid.New(), Todo{Task: ""})
-	assert.Nil(t, err)
-	assert.Nil(t, todo)
+	withRollback(t, func(tx *sql.Tx) {
+		r := NewTodosRepository(tx)
+		todo, err := r.UpdateOne(uuid.New(), Todo{Task: ""})
+		assert.Nil(t, err)
+		assert.Nil(t, todo)
+	})
 }
 
 func TestCreatesOneTodo(t *testing.T) {
-	r := NewTodosRepository(startTransaction(t))
-	todo, err := r.CreateOne(Todo{Task: "Learn Go"})
-	assert.Nil(t, err)
-	assert.Equal(t, todo.Task, "Learn Go")
+	withRollback(t, func(tx *sql.Tx) {
+		r := NewTodosRepository(tx)
+		todo, err := r.CreateOne(Todo{Task: "Learn Go"})
+		assert.Nil(t, err)
+		assert.Equal(t, todo.Task, "Learn Go")
+	})
 }
 
 func TestNonEmptyTableGetsOneTodo(t *testing.T) {
-	tx := startTransaction(t)
-	r := NewTodosRepository(tx)
-	id := uuid.New()
-	_, err := tx.Exec(`--sql
-		insert into "todos" ("todoId", "task")
-		values ($1, 'Learn Go')
-	`, id)
-	assert.Nil(t, err)
-	todo, err := r.GetOneByID(id)
-	assert.Nil(t, err)
-	assert.Equal(t, todo.Task, "Learn Go")
-
+	withRollback(t, func(tx *sql.Tx) {
+		r := NewTodosRepository(tx)
+		id := uuid.New()
+		_, err := tx.Exec(`--sql
+			insert into "todos" ("todoId", "task")
+			values ($1, 'Learn Go')
+		`, id)
+		assert.Nil(t, err)
+		todo, err := r.GetOneByID(id)
+		assert.Nil(t, err)
+		assert.Equal(t, todo.Task, "Learn Go")
+	})
 }
 
 func TestNonEmptyTableUpdatesOneTodo(t *testing.T) {
-	tx := startTransaction(t)
-	r := NewTodosRepository(tx)
-	id := uuid.New()
-	_, err := tx.Exec(`--sql
-		insert into "todos" ("todoId", "task")
-		values ($1, 'Learn Go')
-	`, id)
-	assert.Nil(t, err)
-	todo, err := r.UpdateOne(id, Todo{Task: "Accept Go"})
-	assert.Nil(t, err)
-	assert.Equal(t, todo.Task, "Accept Go")
+	withRollback(t, func(tx *sql.Tx) {
+		r := NewTodosRepository(tx)
+		id := uuid.New()
+		_, err := tx.Exec(`--sql
+			insert into "todos" ("todoId", "task")
+			values ($1, 'Learn Go')
+		`, id)
+		assert.Nil(t, err)
+		todo, err := r.UpdateOne(id, Todo{Task: "Accept Go"})
+		assert.Nil(t, err)
+		assert.Equal(t, todo.Task, "Accept Go")
+	})
 }
